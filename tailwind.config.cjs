fd0ee2b81dc8878e80194435f1796ae6d6733c35
Claudio Salatino <claudiopome@gmail.com>
2022-10-16 17:00:11 +0200

    add fontsize customization to tailwind conf.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,14 @@ module.exports = {
   ],
   theme: {
     fontSize: {
+      sm: ['0.87rem', '1.25rem'],
+      base: ['1rem', '1.5rem'],
+      lg: ['1.125rem', '1.75rem'],
+      xl: ['1.25rem', '2rem'],
+      '2xl': ['1.5rem', '2rem'],
+      '3xl': ['1.875rem', '2.25rem'],
+      '4xl': ['2.25rem', '2.5rem'],
+      '5xl': ['3rem', '1'],
       '6xl': ['3.75rem', '1.2'],
     },
     fontFamily: {
