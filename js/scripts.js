10f574f171e04ca2c411fac71bdd5535886911f6
Claudio Salatino <claudiopome@gmail.com>
2022-11-22 16:17:24 +0100

    wip: mobile menu js improvements.

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,17 @@ window.addEventListener("load", function() {
     const menu =  document.getElementById("mobile-menu");
     const openMenu = document.getElementById("open-menu");
     const closeMenu = document.getElementById("close-menu");
-
-    console.log(openMenu);
-    console.log(menu);
-
+    
     openMenu.addEventListener("click", function(e) {
         e.preventDefault();
-        menu.classList.remove("opacity-0");
-        menu.classList.add("fixed", "opacity-100", "top-0", "right-0", "bottom-0", "left-0");
+        menu.classList.remove("opacity-0", "w-0", "h-0", "p-0");
+        menu.classList.add("fixed", "opacity-100", "top-0", "right-0", "bottom-0", "left-0", "w-screen", "h-screen", "p-8");
     });
 
     closeMenu.addEventListener("click", function(e) {
         e.preventDefault();
-        menu.classList.remove("fixed", "opacity-100", "top-0", "right-0", "bottom-0", "left-0");
-        menu.classList.add("opacity-0");
+        menu.classList.remove("fixed", "opacity-100", "top-0", "right-0", "bottom-0", "left-0", "w-screen", "h-screen", "p-8" );
+        menu.classList.add("opacity-0", "w-0", "h-0", "p-0");
     });
 
 
