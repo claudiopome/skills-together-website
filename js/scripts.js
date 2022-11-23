af3b835a71f44e961ed16632d23ceffc26c89f07
Claudio Salatino <claudiopome@gmail.com>
2022-11-23 17:07:28 +0100

    wip: add comments to js.

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ window.addEventListener("load", function() {
     const closeMenu = document.getElementById("close-menu");
     const openSubMenu = document.getElementById("open-submenu");
 
+    //Mobile menu open.
     openMenu.addEventListener("click", function(e) {
         e.preventDefault();
         menu.classList.remove("opacity-0", "w-0", "h-0", "p-0");
@@ -13,6 +14,7 @@ window.addEventListener("load", function() {
         closeMenu.classList.add("fixed", "top-6");
     });
 
+    //Mobile menu close.
     closeMenu.addEventListener("click", function(e) {
         e.preventDefault();
         menu.classList.remove("fixed", "opacity-100", "top-0", "right-0", "bottom-0", "left-0", "w-screen", "h-screen", "p-8" );
@@ -20,7 +22,7 @@ window.addEventListener("load", function() {
         this.classList.remove("fixed", "top-6");
         
     });
-
+    //Submenu open and close.
     openSubMenu.addEventListener("click", function(e) {
         e.preventDefault();
         subMenu.classList.toggle("hidden");
